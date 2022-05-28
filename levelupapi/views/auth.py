9ab5c684f46a31a8779a39e62f393fe110a7aba6
Steve Nagel <stevencnagel@gmail.com>
2022-05-28 10:48:41 -0500

    ch5_commit

---
+++ levelupapi/views/auth.py
@@ -0,0 +1,48 @@
+from lib2to3.pgen2 import token
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from levelupapi.models import Gamer, gamer
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    username = request.data['username']
+    password = request.data['password']
+    
+    authenticated_user = authenticate(username=username, password=password)
+    
+    if authenticated_user is not None:
+        token = Token.objects.get(user=authenticated_user)
+        data = {
+            'valid': True,
+            'token': token.key
+        }
+        return Response(data)
+    else:
+        data = { 'valid': False }
+        return Response(data)
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    
+    new_user = User.objects.create_user(
+        username=request.data['username'],
+        password=request.data['password'],
+        first_name=request.data['first_name'],
+        last_name=request.data['last_name'],
+    )
+    
+    gamer = Gamer.objects.create(
+        bio=request.data['bio'],
+        user=new_user
+    )
+    
+    token = Token.objects.create(user=new_user)
+    data = { 'token': token.key }
+    return Response(data)
